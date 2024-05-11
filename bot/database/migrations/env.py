a67dd40988992d52ceec77ecb7426cea8087b214
dcdnc <kuunii06@gmail.com>
2024-05-12 01:06:03 +0700

    completed tests

--- bot/database/migrations/env.py
+++ bot/database/migrations/env.py
@@ -1,13 +1,11 @@
 import asyncio
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from bot.config_reader import parse_config, Config
-from bot.database.base import Base
+from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,14 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
-bot_config: Config = parse_config()
-
-config.set_main_option(
-    'sqlalchemy.url',
-    str(bot_config.db_url)
-)
-
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
