b8c702a69f97048de50adbf9cc7962db074209f4
dcdnc <kuunii06@gmail.com>
2024-05-13 15:44:41 +0700

    add antiflood middleware

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 
 from environs import Env
-from pydantic import SecretStr, PostgresDsn, DirectoryPath
+from pydantic import SecretStr, PostgresDsn, DirectoryPath, PositiveInt, PositiveFloat
 from pydantic_settings import BaseSettings
 
 env = Env()
@@ -17,13 +17,14 @@ class Config(BaseSettings):
     webhook_url: str
     webhook_path: str
     port: int
+    flood_awaiting: PositiveFloat
     telegram_secret_token: str
     locales_path: DirectoryPath
     admins: list[int]
     stations_list: list[str]
     star_stations: list[str]
-    star_station_points: int
-    usual_station_points: int
+    star_station_points: PositiveInt
+    usual_station_points: PositiveInt
 
     # model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
@@ -35,4 +36,4 @@ def parse_config() -> Config:
 
 if __name__ == "__main__":
     config = parse_config()
-    print(config.webhook_url)
+    print(config)
